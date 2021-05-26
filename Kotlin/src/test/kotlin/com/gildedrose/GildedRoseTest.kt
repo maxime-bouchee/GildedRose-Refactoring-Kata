90120394dcadf8081e0ac70ee0abd6ab04d6f066
Muhammad <muhammad.jaufurally@octo.com>
2021-05-26 14:46:54 +0200

    wip

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -6,14 +6,31 @@ import org.junit.jupiter.api.Test
 internal class GildedRoseTest {
 
     @Test
-    fun foo() {
+    fun `check quality foo when quality is 0`() {
         val items = arrayOf<Item>(Item("foo", 0, 0))
         val app = GildedRose(items)
         app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
+        assertEquals("foo", app.items[0].name)
 
     }
 
+    @Test
+    fun `check quality foo when quality is 10`() {
+        val items = arrayOf<Item>(Item("foo", 5, 10))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(9, app.items[0].quality)
+
+    }
+
+    @Test
+    fun `check quality Aged brie when quality is less than 50`() {
+        val items = arrayOf<Item>(Item("Aged Brie", 5, 10))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(11, app.items[0].quality)
+
+    }
 }
 
 
