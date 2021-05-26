c645bd9c6d02fb2c7dcf65beb63aa79d5769cdd9
Muhammad <muhammad.jaufurally@octo.com>
2021-05-26 16:51:40 +0200

    wip

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -10,12 +10,19 @@ class GildedRose(var items: Array<Item>) {
     fun updateQuality() {
         for (i in items.indices) {
 
-/*            when(items[i].name){
-                AGED_BRIE -> return
-                else -> updateQualityWhenQualityGreaterThan0(items[i])
-            }*/
+            when(items[i].name){
+                AGED_BRIE -> updateQualityAgedBrie(items[i])
+                BACKSTAGE -> updateQualityBackstage(items[i])
+                SULFURAS -> updateQualitySulfuras(items[i])
+                else -> {
+                    decreaseItemQuality(items[i])
+                    if(items[i].sellIn < 0){
+                        decreaseItemQuality(items[i])
+                    }
+                }
+            }
 
-            if (isNotAgedBrie(items[i].name) && isNotBackstage(items[i].name)) {
+/*            if (isNotAgedBrie(items[i].name) && isNotBackstage(items[i].name)) {
                 updateQualityWhenQualityGreaterThan0(items[i])
             } else {
                 updateQualityWhenQualityLessThan50(items[i])
@@ -24,19 +31,51 @@ class GildedRose(var items: Array<Item>) {
             if (isNotSulfuras(items[i].name)) {
                 decreaseItemSellIn(items[i])
             }
-            updateQualityWhenSellInLessThanZero(items[i])
+            updateQualityWhenSellInLessThanZero(items[i])*/
         }
     }
 
-    private fun updateQualityWhenQualityGreaterThan0(item: Item) {
+/*    private fun updateQualityWhenQualityGreaterThan0(item: Item) {
         if (item.quality > 0) {
             if (isNotSulfuras(item.name)) {
                 decreaseItemQuality(item)
             }
         }
+    }*/
+
+    private fun updateQualityAgedBrie(item: Item){
+        increaseItemQuality(item)
+        decreaseItemSellIn(item)
+        if(item.sellIn < 0){
+            increaseItemQuality(item)
+        }
     }
 
-    private fun updateQualityWhenQualityLessThan50(item: Item) {
+    private fun updateQualitySulfuras(item: Item){
+        increaseItemQuality(item)
+    }
+
+    private fun updateQualityBackstage(item: Item){
+        if(item.quality < 50){
+            increaseItemQuality(item)
+        }
+        if(item.sellIn < 11){
+            if(item.quality < 50){
+                increaseItemQuality(item)
+            }
+        }
+        if(item.sellIn < 6){
+            if(item.quality < 50){
+                increaseItemQuality(item)
+            }
+        }
+        decreaseItemSellIn(item)
+        if(item.sellIn < 0){
+            item.quality = 0
+        }
+    }
+
+/*    private fun updateQualityWhenQualityLessThan50(item: Item) {
         if (item.quality < 50) {
             increaseItemQuality(item)
 
@@ -70,23 +109,27 @@ class GildedRose(var items: Array<Item>) {
                 }
             }
         }
-    }
+    }*/
 
     private fun isNotSulfuras(name: String): Boolean = name != SULFURAS
 
-    private fun isNotBackstage(name: String): Boolean = name != BACKSTAGE
+/*    private fun isNotBackstage(name: String): Boolean = name != BACKSTAGE
 
-    private fun isNotAgedBrie(name: String): Boolean = name != AGED_BRIE
+    private fun isNotAgedBrie(name: String): Boolean = name != AGED_BRIE*/
 
     private fun decreaseItemQuality(item: Item) {
-        item.quality = item.quality - 1
-        if(item.name == CONJURED){
+        if (item.quality > 0) {
             item.quality = item.quality - 1
+            if (item.name == CONJURED) {
+                item.quality = item.quality - 1
+            }
         }
     }
 
     private fun increaseItemQuality(item: Item) {
-        item.quality = item.quality + 1
+        if (item.quality < 50) {
+            item.quality = item.quality + 1
+        }
     }
 
     private fun decreaseItemSellIn(item: Item) {
